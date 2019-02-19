8f8b334c0562925e34eefccb39ea54dc2049d717
zhongchai <z.chai@students.clark.edu>
2019-02-18 19:35:52 -0800

    Changed sticky field to display the correct input, and added comments

--- inc/create/form.inc.php
+++ inc/create/form.inc.php
@@ -5,12 +5,13 @@
 Scroll down the page
 http://php.net/manual/en/language.operators.comparison.php#language.operators.comparison.ternary
 -->
+<!-- Displays the form that allows users to post data to the database, with sticky fields  -->
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
     <label class="col-form-label" for="first">First Name </label>
     <input class="form-control" type="text" id="first" name="first" value="<?php echo (isset($first) ? $first: '');?>">
     <br>
     <label class="col-form-label" for="last">Last Name </label>
-    <input class="form-control" type="text" id="last" name="last" value="<?php echo (isset($last) ? $first: '');?>"">
+    <input class="form-control" type="text" id="last" name="last" value="<?php echo (isset($last) ? $last: '');?>"">
     <br>
     <label class="col-form-label" for="id">Student ID </label>
     <input class="form-control" type="text" id="id" name="id" value="<?php echo (isset($id) ? $id: '');?>"">
--- inc/display/content.inc.php
+++ inc/display/content.inc.php
@@ -6,6 +6,7 @@ require __DIR__ . "/../functions/functions.inc.php";
 $orderby = 'last_name';
 $filter = '';
 
+// read the query from the url to get the letter to filter results by and how to sort them, store them in variables
 if (isset($_GET['filter'])) {
     $filter = $_GET['filter'];
 }
@@ -18,13 +19,15 @@ if (isset($_GET['clearfilter'])){
     $filter = '';
 }
 
+// use the variables obtained from the url to query the database
 $sql = "SELECT * FROM $db_table WHERE last_name LIKE '$filter%' ORDER BY $orderby ASC";
 
 $result = $db->query($sql);
 
+// check if any results exist for the letter specified, if not, display error message
 if ($result->num_rows == 0) {
     echo "<h2 class=\"mt-4 alert alert-warning\">No Records for <strong>last names</strong> starting with <strong>$filter</strong></h2>";
-} else {
+} else { // otherwise, display the number of results in the alert box at the top of the page
     if(empty($filter)){
         $text = '';
     } else {
